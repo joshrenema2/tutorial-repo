1c74843bf86fad04be97363c169906d1e43e92ae
joshrenema2 <60946895+joshrenema2@users.noreply.github.com>
2020-04-05 20:20:48 -0700

    save 2

---
+++ stories/CustomButton.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CustomButton from "../comps/CustomButton";
+
+
+export default {
+    title:"Custom Button",
+    component:CustomButton,
+};
+
+export const MyCustomButton = () => <CustomButton />;
